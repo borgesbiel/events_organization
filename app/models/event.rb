550a264983989cc7e7faf67b92365c96f24a465a
Gabriel Borges Oliveira <gabriel.bidula@gmail.com>
2015-12-22 14:55:54 +0000

    Add User to Events relation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,11 @@
 class Event < ActiveRecord::Base
+  belongs_to :user
+  
+  #paperclip
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  #paperclip
   
+  #order events desc
   default_scope { order(event_date: :desc) }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151222140529) do
+ActiveRecord::Schema.define(version: 20151222145118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,10 @@ ActiveRecord::Schema.define(version: 20151222140529) do
     t.datetime "image_updated_at"
     t.decimal  "lat"
     t.decimal  "lng"
+    t.integer  "user_id"
   end
+
+  add_index "events", ["user_id"], name: "index_events_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "fname"
@@ -39,4 +42,5 @@ ActiveRecord::Schema.define(version: 20151222140529) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "events", "users"
 end
