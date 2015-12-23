0b0ff4696bd095c2d1d147c9c19728ffdb59fd09
Gabriel Borges Oliveira <gabriel.bidula@gmail.com>
2015-12-23 02:10:20 +0000

    Add iteration for each user events in user index , comment out edit and new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @events = Event.where(user_id: @user.id).order(event_date: :asc)
   end
 
   def new
