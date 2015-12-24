85c2e2d42fa99120da05ac0d12320d6d4e93efaa
Gabriel Borges Oliveira <gabriel.bidula@gmail.com>
2015-12-24 04:07:18 +0000

    Added style and css for tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,19 +15,19 @@ p "Seeding Events for user_id = 1"
   Event.create(        
     name: "Eminem",
     image: "https://consequenceofsound.files.wordpress.com/2013/10/eminem.jpg",
-    address: "Allianz Parque, Sao Paulo",
+    address: "Pinheiros, Sao Paulo",
     event_date: "2016-04-07",
-    latitude: -23.5505199,
-    longitude: -46.6333094,
+    latitude: -23.5619862,
+    longitude: 46.6971976,
     user_id: 1
   )
   Event.create(        
     name: "Florence",
     image: "http://static.guim.co.uk/sys-images/Guardian/Pix/pictures/2015/2/12/1423759757798/6a8c828c-41ef-43aa-bb73-75d86aee1c00-2060x1236.jpeg",
-    address: "Autodromo de interlagos, Sao Paulo",
+    address: "Osasco, Sao Paulo",
     event_date: "2016-03-12",
-    latitude: -23.7062075,
-    longitude: -46.6883859,
+    latitude: -23.5328838,
+    longitude: -46.8545641,
     user_id: 1
   )
 p "Events seeded"  
